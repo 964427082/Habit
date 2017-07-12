12b96850c4867c7a6ab6ef5676c9afc099b85f1c
964427082 <964427082@qq.com>
2017-07-12 16:18:13 +0800

    1、添加习惯界面 2、新习惯界面

--- app/src/main/java/f3/nsu/com/habit/acitvity/AddHabitActivity.java
+++ app/src/main/java/f3/nsu/com/habit/acitvity/AddHabitActivity.java
@@ -12,6 +12,8 @@ import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import f3.nsu.com.habit.R;
@@ -30,6 +32,8 @@ public class AddHabitActivity extends FragmentActivity implements View.OnClickLi
     NewHabitFragment newHabitFragment;//新习惯界面
     AddHabitFragment addHabitFragment;//添加习惯界面
     private EditText nameEditText,dayEditText,wordsEditText;//新习惯界面名称、天数、语录的编辑框
+    private Button color_button1,color_button2,color_button3,color_button4,color_button5;//五个颜色选择按钮
+    private RadioButton radioButton1,radioButton2,radioButton3,radioButton4,radioButton5;//五个颜色单选按钮
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -46,6 +50,24 @@ public class AddHabitActivity extends FragmentActivity implements View.OnClickLi
         createHabit_button = (Button) findViewById(R.id.createHabit_button);
         exit_imageButton = (ImageButton) findViewById(R.id.exit_imageButton);
         return_imageButton = (ImageButton) findViewById(R.id.return_imageButton);
+
+        radioButton1 = (RadioButton) findViewById(R.id.radioButton1);
+        radioButton2 = (RadioButton) findViewById(R.id.radioButton2);
+        radioButton3 = (RadioButton) findViewById(R.id.radioButton3);
+        radioButton4 = (RadioButton) findViewById(R.id.radioButton4);
+        radioButton5 = (RadioButton) findViewById(R.id.radioButton5);
+        color_button1 = (Button) findViewById(R.id.color_button1);
+        color_button2 = (Button) findViewById(R.id.color_button2);
+        color_button3 = (Button) findViewById(R.id.color_button3);
+        color_button4 = (Button) findViewById(R.id.color_button4);
+        color_button5 = (Button) findViewById(R.id.color_button5);
+        color_button1.setOnClickListener(this);
+        color_button2.setOnClickListener(this);
+        color_button3.setOnClickListener(this);
+        color_button4.setOnClickListener(this);
+        color_button5.setOnClickListener(this);
+
+
         nameEditText  = (EditText) findViewById(R.id.name_editText);
         dayEditText = (EditText) findViewById(R.id.day_editText);
         wordsEditText = (EditText) findViewById(R.id.words_editText);
@@ -82,6 +104,21 @@ public class AddHabitActivity extends FragmentActivity implements View.OnClickLi
                 break;
             case R.id.return_imageButton:
                 getSupportFragmentManager().beginTransaction().hide(newHabitFragment).show(addHabitFragment).commit();
+            case R.id.color_button1:
+                radioButton1.setChecked(true);
+                break;
+            case R.id.color_button2:
+                radioButton2.setChecked(true);
+                break;
+            case R.id.color_button3:
+                radioButton3.setChecked(true);
+                break;
+            case R.id.color_button4:
+                radioButton4.setChecked(true);
+                break;
+            case R.id.color_button5:
+                radioButton5.setChecked(true);
+                break;
             default:
                 break;
         }
